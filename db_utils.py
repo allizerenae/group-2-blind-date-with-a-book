5de024ab6741ed714763c0f1c91a70fe6f342376
Nemi <iimoh011@gmail.com>
2025-08-08 01:20:15 +0100

    DB_utils attempt 1 - Nemi

---
+++ db_utils.py
@@ -0,0 +1,81 @@
+import mysql.connector
+from config import USER, PASSWORD, HOST, DATABASE
+
+
+class DbConnectionError(Exception):
+    pass
+
+
+def _connect_to_db():
+    cnx = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        auth_plugin='mysql_native_password',
+        database=DATABASE,
+        port=3306
+    )
+    return cnx
+
+
+def view_latest_book_db():
+    db_connection = None
+    try:
+        db_connection = _connect_to_db()
+        cur = db_connection.cursor()
+        print("Connected to DB: %s" % DATABASE)
+
+        query = """SELECT * FROM orders ORDER BY id DESC LIMIT 1;"""
+        cur.execute(query)
+        result = cur.fetchone()  # Single tuple for the Latest row
+
+        cur.close()
+
+        return result
+
+    except Exception:
+        raise DbConnectionError("Failed to read data from DB")
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+
+def add_new_book_db(new_book_dict):
+    try:
+        db_connection = _connect_to_db()
+        cur = db_connection.cursor()
+        print("Connected to DB: %s" % DATABASE)
+
+        print("ADD THIS BOOK TO DB:", new_book_dict)
+
+        query = """ INSERT INTO BookHistory (Book_title, Author, Genre, Assigned_date, Deadline) VALUES"
+                  (%s, %s, %s, %s, %s) """
+
+        title = new_book_dict["Book_title"]
+        author = new_book_dict["Author"]
+        genre = new_book_dict["Genre"]
+        assigned_date = new_book_dict["Assigned_date"]
+        deadline = new_book_dict["Deadline"]
+
+        cur.execute(query, (Book_title, Author, Genre, Assigned_date, Deadline))
+
+        # Commit the transaction to make the changes in the database
+        db_connection.commit()
+
+        print("Book added successfully!")
+
+
+    except Exception as e:
+        print(e)
+        raise DbConnectionError("Failed to read data from DB")
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+
+if __name__ == "__main__":
+    print("TESTING DB CONNECTION")

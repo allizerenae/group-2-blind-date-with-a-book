367b5de65df15a0ecda9c96f3433616b7f6ecd0e
Nemi <iimoh011@gmail.com>
2025-08-10 10:39:17 +0100

    DB_utils attempt 4. Update SQL name for presumed new SQL Database and table names. Added in config/py and view_all_books functions - Nemi

---
+++ config.py
@@ -0,0 +1,4 @@
+USER = "root"
+PASSWORD = "<Enter SQL Password here>"
+HOST = "localhost"
+DATABASE = "Books"
--- db_utils.py
+++ db_utils.py
@@ -18,6 +18,30 @@ def _connect_to_db():
     return cnx
 
 
+def view_all_books_db():
+    db_connection = None
+    try:
+        db_connection = _connect_to_db()
+        cur = db_connection.cursor()
+        print(f"Connected to DB: {DATABASE}")
+
+        query = """SELECT * FROM Books"""
+        cur.execute(query)
+        result = cur.fetchall()  # this is a list with db records where each record is a tuple
+
+        cur.close()
+
+        return result
+
+    except Exception:
+        raise DbConnectionError("Failed to read data from DB")
+
+    finally:
+        if db_connection:
+            db_connection.close()
+            print("DB connection is closed")
+
+
 def view_latest_book_db():
     db_connection = None
     try:
@@ -25,7 +49,7 @@ def view_latest_book_db():
         cur = db_connection.cursor()
         print(f"Connected to DB: {DATABASE}")
 
-        query = """SELECT * FROM BookHistory ORDER BY pk_history_id DESC LIMIT 1;"""
+        query = """SELECT * FROM Books ORDER BY book_id DESC LIMIT 1;"""
         cur.execute(query)
         result = cur.fetchone()  # Single tuple for the Latest row
 
@@ -50,17 +74,16 @@ def add_new_book_db(new_book_dict):
 
         print("ADD THIS BOOK TO DB:", new_book_dict)
 
-        query = """ INSERT INTO BookHistory (Fk_member_id, book_title, author, genre, assigned_date, deadline) VALUES
+        query = """ INSERT INTO BookHistory (book_title, author, genre, assigned_date, deadline) VALUES
                   (%s, %s, %s, %s, %s, %s) """
 
-        fk_member_id = new_book_dict["fk_member_id"]
         title = new_book_dict["book_title"]
         author = new_book_dict["author"]
         genre = new_book_dict["genre"]
         assigned_date = new_book_dict["assigned_date"]
         deadline = new_book_dict["deadline"]
 
-        cur.execute(query, (fk_member_id, title, author, genre, assigned_date, deadline))
+        cur.execute(query, (title, author, genre, assigned_date, deadline))
 
         # Commit the transaction to make the changes in the database
         db_connection.commit()

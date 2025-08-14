a3ceb5c962b302bbc3e81fb1b2f5abf4c95dfa60
maisi <maisie.pepper@outlook.com>
2025-08-14 10:49:16 +0100

    Update book and flask API files and add to requirements file.

--- flask_api.py
+++ flask_api.py
@@ -3,21 +3,25 @@ from db_utils import view_all_books_db, view_latest_book_db, add_new_book_db
 
 app = Flask(__name__)
 
+
 @app.route("/books", methods=["GET"])
 def view_all_books_api():
     return jsonify(view_all_books_db())
 
+
 @app.route("/books/current", methods=["GET"])
 def view_latest_book_api():
     return jsonify(view_latest_book_db())
+
 
 @app.route("/books/add", methods=["POST"])
 def add_new_book_api():
     new_book = request.get_json()
     return jsonify(add_new_book_db(new_book))
 
+
 # Stub for testing API from main.py
-def get_random_book_by_genre(genre):
+def mock_get_random_book_by_genre(genre):
     return {"title": "Mock Book Title", "author": "Mock Author"}
 
 if __name__ == "__main__":

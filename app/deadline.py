aa0fae7dd173ea0538f6abf0d0a9c0ad55c09a8d
Janine OConnor <ja123oconnor@gmail.com>
2025-08-24 10:35:20 +0100

    added manual test to deadline file

--- app/deadline.py
+++ app/deadline.py
@@ -1,11 +1,15 @@
 from datetime import date, timedelta
 
-
+#gets today's date
 def get_assigned_date():
     current_date = date.today()
     return current_date
 
-
+#gets the reading deadline date
 def get_deadline():
     book_deadline = date.today() + timedelta(days=7)
     return book_deadline
+
+#if __name__ == "__main__":
+    #manual testing
+    #print(get_deadline() - get_assigned_date())
